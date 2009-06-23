17ce91ac7a1fb6f954fc90cdfcc4eb0e19aebcb8
Pedro Matiello <pmatiello@gmail.com>
2009-06-23 13:51:42 -0300

    Per commiter stats.

--- bin/git-report.rb
+++ bin/git-report.rb
@@ -9,13 +9,26 @@ require 'date'
 workdir = ARGV[0]
 if (workdir)
   puts "Git stats for repositories under #{workdir}"
+
   reporter = CaelumGitReports.new(workdir)
   reporter.extract_all_stats(Date.new - 365)
-  for repository in reporter.stats.keys
+
+  for repository in reporter.repository_stats.keys
     puts "[Project #{repository}]"
-    for commiter, lines in reporter.stats[repository]
+    for commiter, lines in reporter.repository_stats[repository]
       puts "\t#{commiter}: #{lines}"
     end
     puts
   end
+
+  puts
+
+  for commiter in reporter.commiter_stats.keys
+    puts "[Commiter #{commiter}]"
+    for repository, lines in reporter.commiter_stats[commiter]
+      puts "\t#{repository}: #{lines}"
+    end
+    puts
+  end
+
 end
--- lib/caelum-git-reports.rb
+++ lib/caelum-git-reports.rb
@@ -4,29 +4,39 @@ require 'date'
 class CaelumGitReports
   attr_accessor :work_dir
   attr_reader :repositories
-  attr_accessor :stats
+  attr_accessor :repository_stats, :commiter_stats
 
   def initialize(work_dir)
     @work_dir = work_dir
     @repositories = Hash.new
-    @stats = Hash.new
+    @repository_stats = Hash.new
+    @commiter_stats = Hash.new
     discover_repositories
     initialize_repositories
   end
 
   def extract_stats(repository_name, from = Date.new, to = Date.new)
-    @stats[repository_name] = Hash.new
+    @repository_stats[repository_name] = Hash.new
     repository = @repositories[repository_name]
     repository.pull
     repository.calculate_stats(from, to)
     for commiter in repository.commiters.keys
-      @stats[repository_name][commiter] = repository.commiters[commiter]
+      @repository_stats[repository_name][commiter] = repository.commiters[commiter]
     end
+    extract_stats_for_commiters(repository_name)
   end
 
   def extract_all_stats(from = Date.new, to = Date.new)
     for repository in @repositories.values
       extract_stats(repository.name, from, to)
+    end
+  end
+
+  private
+  def extract_stats_for_commiters(repository_name)
+    for commiter, lines in @repository_stats[repository_name]
+      @commiter_stats[commiter] = Hash.new unless @commiter_stats[commiter].instance_of? Hash
+      @commiter_stats[commiter][repository_name] = lines
     end
   end
 
