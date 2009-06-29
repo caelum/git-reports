c4b0084adc7bdb1d3b5b8679ceeea1f8f06bf0f3
Pedro Matiello <pmatiello@gmail.com>
2009-06-29 17:06:39 -0300

    Syntax help for git-report.rb A few code comments.

--- bin/git-report.rb
+++ bin/git-report.rb
@@ -1,21 +1,25 @@
 #!/usr/bin/env ruby
 
+# Load path
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+# Requires
 require 'reporter'
 require 'date'
 require 'html_report'
 require 'html_summary'
 
+# Command line arguments
 workdir = ARGV[0]
 days = ARGV[1].to_i
 max_commits = ARGV[2].to_i
-if (max_commits == 0)
+if (max_commits <= 0)
   max_commits = 50
 end
 
-if (workdir and days)
+# Action!
+if (workdir and days > 0)
   puts "Git stats for repositories under #{workdir}"
 
   reporter = Reporter.new(workdir)
@@ -25,16 +29,20 @@ if (workdir and days)
 
   now = Time.now.strftime("%Y.%m.%d")
 
+  # Repository stats
   repository_html = File.new("repositories-#{now}-#{days}.html", "w")
   repository_html.puts HtmlReport.new(reporter.repository_stats, "Statistics for repositories", days).generate
   repository_html.close
 
+  # Commiter stats
   commiter_html = File.new("commiters-#{now}-#{days}.html", "w")
   commiter_html.puts HtmlReport.new(reporter.commiter_stats, "Statistics for commiters", days).generate
   commiter_html.close
 
+  # Commit summary
   summary_html = File.new("summary-#{now}-#{days}.html", "w")
   summary_html.puts HtmlSummary.new(reporter.repository_summaries, "Summary", days, max_commits).generate
   summary_html.close
-
+else
+  puts "Usage: git-report.rb <path to repositories> <number of days to look back> [<number of commits in summary>]"
 end
